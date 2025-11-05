7efcdfccb5c76a939768164b7bfc5bd00bfc26eb
frankourr <frankourru.22@gmail.com>
2025-11-05 12:54:19 -0300

    la papa

--- mercado/sucursal/models.py
+++ mercado/sucursal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Sucursal(models.Model):
+    provincia = models.CharField(max_length=50, blank=False)
+    ciudad = models.CharField(max_length=50, blank=False)
+    direccion = models.CharField(max_length=50, blank=False)
+    nombre = models.CharField(max_length=50)
+    contacto = models.CharField(max_length=50)
+
+class Empleado(models.Model):
+    class Cargo(models.TextChoices):
+        GERENTE = 'GER', 'Gerente'
+        EMPLEADO = 'EMP', 'Empleado'
+
+    nombre = models.CharField(max_length=20, blank=False)
+    apellido = models.CharField(max_length=20, blank=False)
+    dni = models.IntegerField(max_length=9, unique=True, blank=False)
+    direccion = models.CharField(max_length=50)
+    telefono = models.IntegerField(max_length=15, default='')
+    mail = models.CharField(max_length=50, default='')
+    cargo = models.CharField(max_length=3, choices=Cargo.choices)
+    sucursal = models.ForeignKey(Sucursal, related_name='Empleados')
