b37e8ee7b1db4a1fb23d1133b80ece6010b1ddaa
frankourr <frankourru.22@gmail.com>
2025-11-05 13:50:44 -0300

    a

--- mercado/products/models.py
+++ mercado/products/models.py
@@ -1,6 +1,25 @@
 from django.db import models
-
+from sucursal.models import Sucursal
 # Create your models here.
+
+
+class Catalogo(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    sucursal = models.ForeignKey(Sucursal, related_name='CatalogoProductos', on_delete=models.DO_NOTHING)
+
+class CategoriaProducto(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    catalogo = models.ForeignKey(Catalogo, related_name='Categoria', on_delete=models.DO_NOTHING)
+
+class SubCategoriaProducto(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    categoria = models.ForeignKey(Catalogo, related_name='Subcategoria', on_delete=models.DO_NOTHING)
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -10,21 +29,8 @@ class Product(models.Model):
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class CategoriaProducto(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class SubCategoriaProducto(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Catalogo(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    categoria = models.ForeignKey(CategoriaProducto, related_name='Producto', on_delete=models.DO_NOTHING) 
+    subcategoria = models.ForeignKey(SubCategoriaProducto, related_name='Producto', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
--- mercado/products/views.py
+++ mercado/products/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializer import ProductSerializer
-from .models import Product
+from .models import Product, Catalogo, CategoriaProducto, SubCategoriaProducto
 
 # Create your views here.
 
+class CatalogoViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Catalogo.objects.all()
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = CategoriaProducto.objects.all()
+
+class SubCategoriaViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = SubCategoriaProducto.objects.all()
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
